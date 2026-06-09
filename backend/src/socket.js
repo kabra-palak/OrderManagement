02f25fef0576fb91390d934a6e9e527ad27e56f8
kabra-palak <kabrapalak08@gmail.com>
2026-06-09 23:30:21 +0530

    updated socket cors origin

--- backend/src/socket.js
+++ backend/src/socket.js
@@ -5,7 +5,7 @@ let io;
 export const initSocket = (httpServer) => {
   io = new Server(httpServer, {
     cors: {
-      origin: "http://localhost:3000",
+      origin: process.env.CLIENT_URL,
       methods: ["GET", "POST", "PATCH"],
     },
   });
